ba979b1a4aa34e6f609a962e1b4dec316a6312ae
Hridoy75-hub <hcsarker2002@gmail.com>
2021-05-26 22:26:41 +0600

    Update index.js

--- Practise1/index.js
+++ Practise1/index.js
@@ -18,11 +18,14 @@ mongoose.connection.once('open', () => {
 })
 
 app.use('/vehe', Veherouter)
-app.use('/dog',Dogrouter)
+app.use('/dog', Dogrouter)
+
+//string response
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//json response
 app.get('/about', (req, res) => {
 
   var myjson = [
@@ -40,14 +43,49 @@ app.get('/about', (req, res) => {
     name: "Dipti",
     city: "Tangail",
     occupation:"Houswife"
-
     }
-   
-    
-
   ]
   res.json(myjson)
 })
+//download response
+app.get('/upload', (req, res) => {
+  res.download("./Database.js")
+})
+
+//redirect response
+app.get('/Bangladesh', (req, res) => {
+  res.redirect("http://localhost:5000/India")
+})
+
+app.get('/India', (req, res) => {
+  res.send("This is India")
+})
+
+//header response
+
+//response cookie
+app.get('/cookie', (req, res) => {
+
+  res.cookie("name", "Hridoy")
+  res.cookie("city", "Dhaka")
+  res.cookie("age", "19 year")
+  
+  res.end("Cookie set succes")
+  
+})
+//clear cookie response
+app.get('/cooki', (req, res) => {
+
+  res.clearCookie("name", "Hridoy")
+  
+  res.clearCookie("age", "19 year")
+  
+  res.end("Cookie clear succes")
+  
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
